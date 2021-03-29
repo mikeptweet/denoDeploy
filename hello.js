34a9a0d4225110c785873e39253f23a1fa363d0f
Mike Parsons <mike.r.parsons@gmail.com>
2021-03-29 12:17:58 -0230

    Update hello.js
    Fixed a typo

--- hello.js
+++ hello.js
@@ -1,6 +1,6 @@
 addEventListener("fetch", (event) => {
   //var test = eval("Date()");
-  const response = new Response("Hello World!\n"+Object.keys(this).sort().join("\n")), {
+  const response = new Response("Hello World!\n"+Object.keys(this).sort().join("\n"), {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
