d39a3ef634902b2372b2b30b7a5ef1ac8227147e
Mike Parsons <mike.r.parsons@gmail.com>
2021-03-29 11:35:17 -0230

    Update hello.js
    Added eval test

--- hello.js
+++ hello.js
@@ -1,5 +1,6 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
+  var test = eval("Date()");
+  const response = new Response("Hello World! "+ test, {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
